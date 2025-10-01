0ef4a6e6000d7ce749dbbf1cb9bc14f400c6d3c2
Chia-Le Lin <chllin@cs.nycu.edu.tw>
2025-10-01 11:18:49 +0800

    ✨ feat: clampedExpVecto finish

--- HW1/part1/vectorOP.cpp
+++ HW1/part1/vectorOP.cpp
@@ -50,31 +50,42 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
   // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
   //
   __pp_vec_float x; // every lane value
-  __pp_vec_float y;
-  __pp_vec_float zero = _pp_vset_float(0.f);
-  __pp_vec_float one = _pp_vset_float(1.f);
-  __pp_vec_float nine = _pp_vset_float(9.999999f);
+  __pp_vec_int y;
+  __pp_vec_int zeroI = _pp_vset_int(0);
+  __pp_vec_int oneI  = _pp_vset_int(1);
+  __pp_vec_float clampMax = _pp_vset_float(9.999999f);
   __pp_vec_int count;
   __pp_vec_float result;
-  __pp_mask maskAll, maskEqualZero, maskNotEqualZero, maskLGNine;
+  __pp_mask maskAll, maskEqualZero, maskNotEqualZero, maskClamp, maskDo;
 
   for (int i = 0; i < N; i += VECTOR_WIDTH)
   {
-    maskAll = _pp_init_ones();
+    // every turn lanes, avoid to outbound
+    int lanes = (N - i >= VECTOR_WIDTH) ? VECTOR_WIDTH : (N - i);
+    maskAll = _pp_init_ones(lanes);
+    // Batch Load
     _pp_vload_float(x, values + i, maskAll); // x = values[i];
-    _pp_vload_float(y, exponents + i, maskAll); // y = exponents[i]
-    _pp_veq_float(maskEqualZero, y, zero, maskAll); // if (y == 0)
+    _pp_vload_int(y, exponents + i, maskAll); // int y = exponents[i]
+
+    _pp_veq_int(maskEqualZero, y, zeroI, maskAll); // if (y == 0)
     _pp_vset_float(result, 1.f, maskEqualZero); // output[i] = 1.f;
 
     maskNotEqualZero = _pp_mask_not(maskEqualZero); // else
-    _pp_vadd_float(result, zero, x, maskNotEqualZero); // result = x;
-    _pp_vsub_int(count, y, one, maskNotEqualZero); // int count = y - 1;
-    while() { // while (count > 0)
-      _pp_vmult_float(result, result, x, maskAll); // result *= x;
-      _pp_vsub_int(count, y, one, maskNotEqualZero); // count--;
+    _pp_vmove_float(result, x, maskNotEqualZero); // result = x;
+    _pp_vsub_int(count, y, oneI, maskNotEqualZero); // int count = y - 1;
+    
+    // maskDo : count > zero , so need to do 
+    _pp_vgt_int(maskDo, count, zeroI, maskNotEqualZero);
+    // maskDo = [1,0,0,1] -> _pp_cntbits(maskDo) = 2
+    while(_pp_cntbits(maskDo) > 0) { // while (count > 0)
+      _pp_vmult_float(result, result, x, maskDo); // result *= x;
+      _pp_vsub_int(count, count, oneI, maskDo); // count--;
+      _pp_vgt_int(maskDo, count, zeroI, maskNotEqualZero);
     }
-    _pp_vgt_float(maskLGNine, result, nine, maskAll); // if (result > 9.999999f)
-    _pp_vset_float(result, 9.999999f, maskLGNine); // result = 9.999999f;
+    // result > 9.999999f 
+    _pp_vgt_float(maskClamp, result, clampMax, maskAll); // if (result > 9.999999f)
+    _pp_vmove_float(result, clampMax, maskClamp); // result = 9.999999f;
+    // write back
     _pp_vstore_float(output + i, result, maskAll); // output[i] = result;
   }
 }
