38f6d9e6deba37d7a691437855c8d795299bd531
Chia-Le Lin <chllin@cs.nycu.edu.tw>
2025-10-15 15:02:43 +0800

    ✨ feat: avx2 version
    Signed-off-by: Chia-Le Lin <chllin@cs.nycu.edu.tw>

--- HW2/part1/pi_no_avx2.c
+++ HW2/part1/pi_no_avx2.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdint.h>
-#include <immintrin.h>
-#include <stdalign.h> 
 
 
 typedef struct {
@@ -12,11 +10,12 @@ typedef struct {
     long long int local_hit;
 } Arg;
 
-__m256i xorshift32_vec(__m256i state) {
-    __m256i x = state;
-    x = _mm256_xor_si256(x, _mm256_slli_epi32(x, 13));
-    x = _mm256_xor_si256(x, _mm256_srli_epi32(x, 17));
-    x = _mm256_xor_si256(x, _mm256_slli_epi32(x, 5));
+uint32_t xorshift32(uint32_t *state) {
+    uint32_t x = *state;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    *state = x;
     return x;
 }
 
@@ -24,50 +23,21 @@ void *calculate_pi(void *arg) {
     Arg* data = (Arg*) arg;
     long long int local_tosses = data->local_tosses;
     long long int local_hits = 0;
-    
-    uint32_t base = (uint32_t)(time(NULL) ^ (uintptr_t)pthread_self());
-    alignas(32) uint32_t s[8];
-    for(int i = 0; i < 8; i++) {
-        s[i] = base + i * 767796405u;
-    }
-    __m256i state = _mm256_load_si256((const __m256i*)s); // __m256i state = _mm256_setr_epi32(s0, s1, s2, s3, s4, s5, s6, s7);
-    
+    // unsigned int seed = time(NULL) ^ pthread_self(); // xor
+    uint32_t state = (uint32_t)(time(NULL) ^ (uintptr_t)pthread_self());
 
     const float inverse_uint32_max = 1.0f / 4294967296.0f; // 2^32
-    const __m256 v_scale = _mm256_set1_ps(inverse_uint32_max * 2.0f);
-    const __m256 v_bias = _mm256_set1_ps(-1.0f);
-    const __m256 v_one = _mm256_set1_ps(1.0f);
+    const float scale = inverse_uint32_max * 2.0f;
+    const float bias = - 1.0f;
 
-    long long int i = 0;
-    for(; i + 7 < local_tosses; i+=8) {
-      state = xorshift32_vec(state);
-      __m256 x = _mm256_add_ps(_mm256_mul_ps(_mm256_cvtepi32_ps(state), v_scale), v_bias);
-      
-      state = xorshift32_vec(state);
-      __m256 y = _mm256_add_ps(_mm256_mul_ps(_mm256_cvtepi32_ps(state), v_scale), v_bias);
-
-      __m256 distance = _mm256_fmadd_ps(x, x, _mm256_mul_ps(y, y)); 
-
-      __m256 cmp = _mm256_cmp_ps(distance, v_one, _CMP_LE_OQ);
-      int mask =_mm256_movemask_ps(cmp);
-      local_hits += __builtin_popcount(mask);
+    for(long long int i = 0; i < local_tosses; i++) {
+      float x = (float)xorshift32(&state) * scale + bias;
+      float y = (float)xorshift32(&state) * scale + bias;
+      if(x * x + y * y <= 1.0f) {
+        local_hits++;
+      }
     }
 
-    if (i < local_tosses) {
-        uint32_t tail = base ^ 0x9E3779B9u;
-        const float scale = inv2_32 * 2.0f, bias = -1.0f;
-        for (; i < local_tosses; i++) {
-            // x
-            tail ^= tail << 13; tail ^= tail >> 17; tail ^= tail << 5;
-            float xf = (float)tail * scale + bias;
-            // y
-            tail ^= tail << 13; tail ^= tail >> 17; tail ^= tail << 5;
-            float yf = (float)tail * scale + bias;
-
-            local_hits += (xf*xf + yf*yf <= 1.0f);
-        }
-    }
-    
     data->local_hit = local_hits;
     return NULL;
 }
