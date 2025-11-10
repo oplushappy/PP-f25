b11bf13febe962c882d89d84ac7eb27c7282f884
Chia-Le Lin <chllin@cs.nycu.edu.tw>
2025-11-10 10:02:09 +0800

    🎉 init: hw4

---
+++ HW4/part2/matmul.cc
@@ -0,0 +1,28 @@
+#include <mpi.h>
+
+void construct_matrices(
+    int n, int m, int l, const int *a_mat, const int *b_mat, int **a_mat_ptr, int **b_mat_ptr)
+{
+    /* TODO: The data is stored in a_mat and b_mat.
+     * You need to allocate memory for a_mat_ptr and b_mat_ptr,
+     * and copy the data from a_mat and b_mat to a_mat_ptr and b_mat_ptr, respectively.
+     * You can use any size and layout you want if they provide better performance.
+     * Unambitiously copying the data is also acceptable.
+     *
+     * The matrix multiplication will be performed on a_mat_ptr and b_mat_ptr.
+     */
+}
+
+void matrix_multiply(
+    const int n, const int m, const int l, const int *a_mat, const int *b_mat, int *out_mat)
+{
+    /* TODO: Perform matrix multiplication on a_mat and b_mat. Which are the matrices you've
+     * constructed. The result should be stored in out_mat, which is a continuous memory placing n *
+     * l elements of int. You need to make sure rank 0 receives the result.
+     */
+}
+
+void destruct_matrices(int *a_mat, int *b_mat)
+{
+    /* TODO */
+}
