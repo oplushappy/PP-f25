577caf69d9a6e93905d267135aeece4ca7cd9d0d
Chia-Le Lin <chllin@cs.nycu.edu.tw>
2025-11-12 11:18:13 +0800

    ✨ feat: 1.2.4 MPI Collective: MPI_Gather

--- HW4/part1/pi_gather.c
+++ HW4/part1/pi_gather.c
@@ -16,12 +16,41 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // the amount process need to do
+    long long int local_tosses = tosses / world_size;
+    if (world_rank == world_size - 1) {
+        local_tosses += tosses % world_size;
+    }
+
+    // every process do
+    unsigned int seed = (unsigned int)(time(NULL) ^ (world_rank * 1337u + 0x9e3779b9u));
+    long long int local_count = 0;
+    for (long long int i = 0; i < local_tosses; i++) {
+        // rand_r thread safe
+        double x = (double)rand_r(&seed) / (double)RAND_MAX * 2.0 - 1.0;
+        double y = (double)rand_r(&seed) / (double)RAND_MAX * 2.0 - 1.0;
+        double d2 = x * x + y * y;
+        if (d2 <= 1.0) local_count++;
+    }
 
     // TODO: use MPI_Gather
+    // MPI_Gather, MPI_Igather - Gathers values from a group of processes
+    // int MPI_Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm)
+    long long int *counts = NULL;
+    if (world_rank == 0) {
+        counts = (long long int*)malloc(sizeof(long long int) * world_size);
+    }
+    MPI_Gather(&local_count, 1, MPI_LONG_LONG, counts, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
 
     if (world_rank == 0)
     {
         // TODO: PI result
+        long long int total_count = 0;
+        for (int i = 0; i < world_size; i++) total_count += counts[i];
+        pi_result = 4.0 * (double)total_count / (double)tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
